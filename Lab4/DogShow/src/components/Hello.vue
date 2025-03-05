1a6553a658b78106fc57d12c6c933fe5ad4df587
Taymurazzzz <112979718+Taymurazzzz@users.noreply.github.com>
2025-03-05 12:05:05 +0300

    Add files via upload

---
+++ Lab4/DogShow/src/components/Hello.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineExpose({
+  name: "Hello"
+})
+</script>
+
+<template>
+<div>
+  <h1>Привет, это твой первый компонент</h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Lab4/DogShow/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="username" v-model="username" placeholder="Uername">
+    <input type="password" v-model="password" placeholder="Password">
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+defineExpose({
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      axios.post('http://localhost:8000/auth/token/login', {
+        password: this.data.password,
+        username: this.data.username
+      })
+
+
+    }
+  }
+})
+</script>
